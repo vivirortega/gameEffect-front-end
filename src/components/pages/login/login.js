15dadd23119b5e11e7dda3b1fbe07720c5ca2ab6
unverzed <vitoriarortega@gmail.com>
2022-08-16 20:13:07 -0300

    fix: navigate to home

--- src/components/pages/login/login.js
+++ src/components/pages/login/login.js
@@ -26,7 +26,7 @@ export default function Login() {
       setBio(response.data.userinfo.bio);
       setUserId(response.data.userinfo.id);
       localStorage.setItem("token", response.data.token);
-      navigate("/");
+      navigate("/home");
       setLogin("");
       setPassword("");
     });
