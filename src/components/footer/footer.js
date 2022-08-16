88ad945a53236fa4f7799d7080dc30ef78fb4989
unverzed <vitoriarortega@gmail.com>
2022-08-16 20:20:42 -0300

    fix: link to home in footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,17 +3,20 @@ import { FiUser } from "react-icons/fi";
 import { FiPlusCircle } from "react-icons/fi";
 import { HiOutlineHome } from "react-icons/hi";
 import { Link } from "react-router-dom";
+import UserContext from "../../contexts/usercontext";
+import { useContext } from "react";
 
 export default function Footer() {
+  const { userId } = useContext(UserContext);
   return (
     <Background>
-      <Link to="/">
+      <Link to="/home">
         <HiOutlineHome className="home" />
       </Link>
       <Link to="/game">
         <FiPlusCircle className="plus" />
       </Link>
-      <Link to="/user/2">
+      <Link to={`/user/${userId}`}>
         <FiUser className="user" />
       </Link>
     </Background>
